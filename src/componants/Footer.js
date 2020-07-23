675f05a9343941315684c2d49432482be7edfb01
Chafik <chiko360@localhost.localdomain>
2020-07-23 21:41:05 +0100

    changed footer

--- src/componants/Footer.js
+++ src/componants/Footer.js
@@ -1,47 +1,102 @@
 import React from 'react';
 const Footer = () => {
   return (
-<div className="main-footer">
-  <div className="container">
-    <div className="row">
-
-      {/* column1 */}
-      <div class="col-md-5 col-sm-6">
-                    <h1>PFE ESI-SBA</h1>
-                    <p>LOGO</p>
+          <footer className="page-footer font-small unique-color-dark">
+            <div style={{backgroundColor: '#3498db'}}>
+              <div className="container">
+                {/* Grid row*/}
+                <div className="row py-4 d-flex align-items-center">
+                  {/* Grid column */}
+                  <div className="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                    <h6 className="mb-0">Get connected with us on social networks!</h6>
+                  </div>
+                  {/* Grid column */}
+                  {/* Grid column */}
+                  <div className="col-md-6 col-lg-7 text-center text-md-right">
+                    {/* Facebook */}
+                    <a className="fb-ic">
+                      <i className="fab fa-facebook-f white-text mr-4"> </i>
+                    </a>
+                    {/* Twitter */}
+                    <a className="tw-ic">
+                      <i className="fab fa-twitter white-text mr-4"> </i>
+                    </a>
+                    {/* Google +*/}
+                    <a className="gplus-ic">
+                      <i className="fab fa-google-plus-g white-text mr-4"> </i>
+                    </a>
+                    {/*Linkedin */}
+                    <a className="li-ic">
+                      <i className="fab fa-linkedin-in white-text mr-4"> </i>
+                    </a>
+                    {/*Instagram*/}
+                    <a className="ins-ic">
+                      <i className="fab fa-instagram white-text"> </i>
+                    </a>
+                  </div>
+                  {/* Grid column */}
+                </div>
+                {/* Grid row*/}
               </div>
-      {/* column2 */}
-      <div class="col-md-3 col-sm-12">
-               		
-                    <h2>INFOS</h2>
-                    <ul className="links">
-                        <li><a href="http://www.esi-sba.dz/fr/" >L'Ecole National d'Informatique  </a></li>
-                        <li> <a href="http://moodle.esi-sba.dz/"> Site Moodle</a> </li>
-                    </ul>
-   
-                    
-               </div>
-      {/* column3 */}
-      <div class="col-md-4 col-sm-6">
-                    <div class="footer-info">
-                    	<h2>NOUS CONTACTER </h2>
-                         <p>Tel/fax : +213 48 74 94 52</p>
-                         <p>Email : <a href="mailto:info@esi-sba.dz">info@esi-sba.dz</a></p>
-                         <p><a href="https://goo.gl/maps/oTLxtTDrXMePc28u5">Notre Lieu</a></p>
-                    </div>
-               </div>
-      <div class="clearfix"></div>
+            </div>
+            {/* Footer Links */}
+            <div className="container text-center text-md-left mt-5">
+              {/* Grid row */}
+              <div className="row mt-3">
+                {/* Grid column */}
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  {/* Content */}
+                  <h6 className="text-uppercase font-weight-bold">Gestion des Projets ESI SBA</h6>
+                  <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{width: '60px'}} />
+                  <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                    consectetur
+                    adipisicing elit.</p>
+                </div>
+                {/* Grid column */}
+                {/* Grid column */}
 
-               <div class="col-md-12 col-sm-12">
-                    <div class="copyright-text">
-                         <p>Copyright &copy;{new Date().getFullYear()} ESI-SBA </p>
-                    </div>
-               </div>
+                {/* Grid column */}
+                {/* Grid column */}
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  {/* Links */}
+                  <h6 className="text-uppercase font-weight-bold">Useful links</h6>
+                  <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{width: '60px'}} />
+                  <p>
+                    <a href="/login">Your Account</a>
+                  </p>
+                  <p>
+                    <a href="/contactus">Contact us</a>
+                  </p>
+                  <p>
+                    <a href="/aboutus">About us</a>
+                  </p>
 
-    </div>
-  </div>  
-</div>
-  )
+                </div>
+                {/* Grid column */}
+                {/* Grid column */}
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  {/* Links */}
+                  <h6 className="text-uppercase font-weight-bold">Contact</h6>
+                  <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{width: '60px'}} />
+                  <p>
+                    <i className="fas fa-home mr-3" /> EL WIAM, 22016 Sidi Bel Abbés</p>
+                  <p>
+                    <i className="fas fa-envelope mr-3" /> www.esi-sba.dz</p>
+                  <p>
+                    <i className="fas fa-phone mr-3" /> +213 48 74 94 52</p>
+                </div>
+                {/* Grid column */}
+              </div>
+              {/* Grid row */}
+            </div>
+            {/* Footer Links */}
+            {/* Copyright */}
+            <div className="footer-copyright text-center py-3">© 2020 Copyright:
+              <a href="https://esi-sba.dz/"> ESI SBA &#10084;&#65039; </a>
+            </div>
+            {/* Copyright */}
+          </footer>
+    );
 }
 
 export default Footer;
